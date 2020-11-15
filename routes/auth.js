6c392ca871c3e49de52955592dda8acde00b397a
sodizdaroglu <americanlaz@gmail.com>
2020-11-15 11:40:02 +0300

    Express server setup with initial routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // @route GET api/auth
 // @desc  GET logged in user
 // @access Private
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
      res.send('Get logged in user');
 });
 
 // @route POST api/auth
 // @desc  Auth user & get token
 // @access Public
-router.get('/' , (req, res) =>{
+router.post('/' , (req, res) =>{
     res.send('Log in user');
 })
 module.exports = router;
--- routes/contacts.js
+++ routes/contacts.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // @route GET api/contacts
 // @desc  Get all users contacts 
 // @access Private
-router.post('/', (req, res) => { res.send('Get all contacts');
+router.get('/', (req, res) => { 
+    res.send('Get all contacts');
 });
 
 // @route POST api/contacts
@@ -20,6 +21,13 @@ router.post('/' , (req,res) => {
 // @access Private
 router.put('/:id' , (req, res) =>{
     res.send('Update contact');
-})
+});
+
+// @route Delete api/contacts/:id
+// @desc  Delete contact
+// @access Private
+router.delete('/:id' , (req , res) => {
+    res.send('Delete contact') ;
+});
 
 module.exports = router;
