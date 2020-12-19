42b28e6b74658fa54f33cb87765c4ea66dc2ef4f
sodizdaroglu <americanlaz@gmail.com>
2020-12-19 18:47:39 +0300

    Contact Context

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,56 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props =>{
+    const initialState = {
+        contacts: [
+            {
+                id:1,
+                name:"Sara Smith",
+                email:"smith@gmail.com",
+                phone:"123-123-123",
+                type:"professinal"
+
+            }
+
+            
+        ]
+    };
+    const [sate, dispatch] = useReducer (contactReducer, initialState) ;
+
+    //Add Contact
+
+    //Delete Contact
+
+    //Set Current Contact
+
+    //Clear Current Contact
+
+    //Update Contact
+
+    //Filter Contacts
+
+    //Clear Filter
+
+    return (
+        <ContactContext.Provider
+           value = {{
+               contact: state.contacts
+           }} >
+               {props.children}
+        </ContactContext.Provider>
+    )
+};
+
+export default ContactState;
