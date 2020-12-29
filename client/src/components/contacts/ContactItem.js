4e49f9e11b03973083a6ff2148ac3dfe0a6c3799
sodizdaroglu <americanlaz@gmail.com>
2020-12-29 21:56:52 +0300

    Delete Contact From UI

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
  const ContactItem = ({ contact }) => {
+    const contactContext = useContext(ContactContext); 
+    const { deleteContact } = contactContext;
      const {id, name, email, phone, type } = contact ;
+
+     const onDelete = () =>{
+         deleteContact(id);
+     };
 
     return (
         <div className = 'card bg-light'>
@@ -25,7 +32,7 @@ import PropTypes from 'prop-types';
             </ul>
             <p>
                 <button className="btn btn-dark btn-sm">Edit</button>
-                <button className="btn btn-danger btn-sm">Delete</button>
+                <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
             </p>
         </div>
     )
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -50,7 +50,9 @@ const ContactState = props =>{
           dispatch({ type: ADD_CONTACT, payload:contact});
       } ; 
     //Delete Contact
-
+      const deleteContact = id =>{
+          dispatch({ type: DELETE_CONTACT, payload:id})
+      };
     //Set Current Contact
 
     //Clear Current Contact
@@ -65,7 +67,8 @@ const ContactState = props =>{
         <ContactContext.Provider
            value = {{
                contacts: state.contacts,
-               addContact
+               addContact,
+               deleteContact
            }} >
                {props.children}
         </ContactContext.Provider>
