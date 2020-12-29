a8bdbe037fa328d937255513b859238643c95934
sodizdaroglu <americanlaz@gmail.com>
2020-12-29 23:55:30 +0300

    Edit&updat contact

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,21 @@
-import React, {useState,useContext} from 'react';
+import React, {useState,useContext, useEffect} from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () =>{
     const contactContext  = useContext(ContactContext);
+    const { addContact,updateContact, clearCurrent ,current} = contactContext;
+    useEffect (() => {
+      if(current !== null) {
+        setContact(current);
+      }else{
+        setContact({
+          name:'',
+          email:'',
+          phone:'',
+          type:'personal'
+        }) ;
+      }
+    }, [contactContext, current]);
     const [contact,setContact] = useState({
         name:'',
         email:'',
@@ -14,19 +27,24 @@ const ContactForm = () =>{
     const onChange = e =>
       setContact({...contact, [e.target.name]: e.target.value});
 
-      const onSubmit= e =>{
-          e.preventDefault();
-          contactContext.addContact(contact);
-          setContact({
-            name:'',
-            email:'',
-            phone:'',
-            type:'personal'
-          })
+      const onSubmit = e => {
+        e.preventDefault();
+        if (current === null ) {
+          addContact(contact);
+        } else {
+          updateContact(contact);
+        }
+        clearAll();
+      };
+
+    
+
+      const clearAll = () => {
+        clearCurrent();
       }
     return (
         <form onSubmit={onSubmit}>
-        <h2 className='text-primary'>Add Contact</h2>
+        <h2 className='text-primary'>{current ? 'Edit Contact' : 'Add Contact'}</h2>
         <input
           type='text'
           placeholder='name'
@@ -54,8 +72,12 @@ const ContactForm = () =>{
           <input type="radio" name="type" value="professional" checked={type === 'professional'} onChange={onChange} 
           />Professional{' '}
            <div>
-               <input type="submit" value="Add Contact" className="btn btn-prim btn-block"/>
+               <input type="submit" value={current ? 'Edit Contact' : 'Add Contact'} className=
+               "btn btn-primary btn-block"/>
            </div>
+           {current && <div>
+             <button className= " btn btn-light btn-block " onClick={clearAll}>Clear</button>
+             </div>}
         </form>
     )
 }
