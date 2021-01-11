cac3da8b23641a5e1e3424b666cd99b269f9dd64
sodizdaroglu <americanlaz@gmail.com>
2021-01-11 18:43:21 +0300

    alert

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,22 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
-    <ContactState>
-
-    <Router>
-<Fragment>
+     <ContactState>
+      <AlertState>
+        <Router>
+         <Fragment>
       <Navbar />
 <div  className="container">
+  <Alerts/>
   <Switch>
 <Route exact path ='/' component={Home}/>
 <Route exact path ='/about' component={About}/>
@@ -26,9 +28,10 @@ const App = () => {
 <Route exact path ='/login' component={Login}/>
   </Switch>
       </div>
-    </Fragment>
-    </Router>
-    </ContactState>
+        </Fragment>
+       </Router>
+      </AlertState>
+     </ContactState>
     </AuthState>
   );
 }
